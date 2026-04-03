e7f0c3e005a6c1dae9d5a4812be4dfd688718bdb
Kydlai <kudl2017@mail.ru>
2026-04-03 13:26:21 +0300

    dll clearing fix

--- Segment.cpp
+++ Segment.cpp
@@ -2,8 +2,6 @@
 
 using namespace std;
 
-
-
 class Segment {
 private:
     static inline unsigned int count{0};
@@ -11,8 +9,6 @@ private:
     static inline unsigned int default_ptr_segment_size{128};
     static inline unsigned int idCounter{0};
     static inline vector<Segment*> segments{};
-
-    Node *dll_head;
 
     unsigned int* data_segment;
     size_t* ptr_segment;
@@ -27,7 +23,6 @@ public:
         data_segment = (unsigned int*) malloc(sizeof(int) * default_data_segment_size);
         ptr_segment = (size_t*) malloc(sizeof(size_t) * default_ptr_segment_size);
         length = sizeof(int) * default_data_segment_size;
-        dll_head = new Node(data_segment, data_segment + length - 1, EMPTY);
         segments.push_back(this);
     }
 
@@ -35,7 +30,6 @@ public:
         --count;
         free(data_segment);
         free(ptr_segment);
-        dll_head->clear_dll();
         for(int i = 0; i < segments.size(); ++i)
             if(segments[i]->id == this->id){
                 segments.erase(segments.begin() + i);
